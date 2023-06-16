0cd7fb55c99baae18631e4ea2b735ad9b8a2c0b4
Elvis Boves <eboves@gmail.com>
2023-06-16 09:38:01 -0400

    Finish the Average Height problem

--- averageHeight.py
+++ averageHeight.py
@@ -1,1 +1,11 @@
-print("Hello World!")
+student_height = [180, 124, 165, 173, 189, 169, 146]
+sum_heights = 0
+runs = 0
+
+for height in student_height:
+    sum_heights += height
+    runs += 1
+
+average = round(sum_heights/runs)
+
+print(f"The average hight among the students is {average}")
