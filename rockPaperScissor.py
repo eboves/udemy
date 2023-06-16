178d42b19aa38ba8ac669babe8121dfd1a11ec2f
Elvis Boves <eboves@gmail.com>
2023-06-16 07:43:45 -0400

    Finish with rock paper scissor game

--- rockPaperScissor.py
+++ rockPaperScissor.py
@@ -1,4 +1,22 @@
 ######################################### 4.4 ROCK PAPER SCISSORS ######################################
 import random
 
-print("Hello world!")
+selection_list = ["rock", "paper", "scissors"]
+
+choice = int(input("What do you choose? Type 0 for Rock, 1 for paper or 2 for Scissors."))
+
+comp_selection = random.randint(0, len(selection_list) - 1)
+#print(f"Computer picked {selection_list[ran_selection]}")
+
+user_selection = selection_list[choice]
+
+if user_selection == selection_list[comp_selection]:
+    print(f"It's a DRAW! You chose {user_selection} and compuer chose {selection_list[comp_selection]}")
+elif user_selection == "rock" and comp_selection == "scissors":
+    print(f"You WON! You chose {user_selection} and compuer chose {selection_list[comp_selection]}")
+elif user_selection == "scissors" and comp_selection == "paper":
+    print(f"You WON! You chose {user_selection} and compuer chose {selection_list[comp_selection]}")
+elif user_selection == "paper" and comp_selection == "rock":
+    print(f"You WON! You chose {user_selection} and compuer chose {selection_list[comp_selection]}")
+else:
+    print(f"You Loose! You chose {user_selection} and compuer chose {selection_list[comp_selection]}")
