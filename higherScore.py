f896e2c11a92ce636c2b919c270991a8edaac0f1
Elvis Boves <eboves@gmail.com>
2023-06-16 14:22:40 -0400

    Finish highest score problem and did an extra by finding the min as well.

--- higherScore.py
+++ higherScore.py
@@ -1,1 +1,21 @@
-print("Hello World!")
+scores = [78, 65, 89, 86, 55, 91, 64, 89, 100]
+
+
+############################### HIGHER SCORE ###################################
+#higher = scores[0]
+#
+#for score in scores:
+#    if score > higher:
+#        higher = score
+#    
+#print(f"The higher score in the list is: {higher}")
+
+############################### HIGHER SCORE ###################################
+
+lowest = scores[0]
+
+for score in scores:
+    if score < lowest:
+        lowest = score
+
+print(f"The lowest score is: {lowest}")
