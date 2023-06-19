c58db9b251d18a5722f73182b9e6a8a59f67ea7e
Elvis Boves <eboves@gmail.com>
2023-06-19 09:20:31 -0400

    Complete user input

--- passwordGenerator.py
+++ passwordGenerator.py
@@ -6,6 +6,20 @@ symbols = ['!','"','#','$','%','&','\/',"'",'(',')','*','+',',','-','.','/',':',
 
 print("Welcome to PassWord Generator")
 
-letter_input = int(input('How many letters would you like to add?'))
-num_input = int(input('How many numbers would you like to add?'))
-symbols_input = int(input('How many symbols would you like to add?'))
+letter_input = int(input('How many letters would you like to add?\n'))
+num_input = int(input('How many numbers would you like to add?\n'))
+symbols_input = int(input('How many symbols would you like to add?\n'))
+
+sort_list = []
+
+for letter in range(0, (letter_input)):
+    sort_list.append(letters[random.randint(0, len(letters)-1)])
+
+for numb in range(0, (num_input)):
+    sort_list.append(num[random.randint(0, len(num)-1)])
+
+for symb in range(0, (symbols_input)):
+    sort_list.append(symbols[random.randint(0, len(symbols)-1)])
+
+
+print(sort_list)
