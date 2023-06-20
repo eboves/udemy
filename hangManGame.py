131db8ca7847e24943e6f433b1d7f696fdd6e38b
Elvis Boves <eboves@gmail.com>
2023-06-20 07:07:18 -0400

    start working with checkiing if a letter is in the guessed word.

--- hangManGame.py
+++ hangManGame.py
@@ -1,3 +1,15 @@
 import random
 
 word_list = ["camel", "baboon", "food"]
+
+chosen_word = random.choice(word_list)
+
+print(chosen_word)
+
+guess = input("Guess a letter: ").lower()
+
+for l in chosen_word:
+    if guess == l:
+        print("right")
+    else:
+        print("wrong")
