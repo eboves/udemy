b4aa2a998d1d0e9f4b4a332f1e037249b7de979c
Elvis Boves <eboves@gmail.com>
2023-06-20 14:40:20 -0400

    start working on the guess again part of the project.

--- hangManGame.py
+++ hangManGame.py
@@ -1,15 +1,24 @@
 import random
 
+display = []
 word_list = ["camel", "baboon", "food"]
 
 chosen_word = random.choice(word_list)
 
+for _ in range(0, len(chosen_word)):
+    display += "_"
+
+print(display)
 print(chosen_word)
+
 
 guess = input("Guess a letter: ").lower()
 
-for l in chosen_word:
-    if guess == l:
+for l in range(0, len(chosen_word)):
+    if guess == chosen_word[l]:
+        display[l] = chosen_word[l]
         print("right")
     else:
         print("wrong")
+
+print(display)
