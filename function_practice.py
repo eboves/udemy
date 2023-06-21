75580dbc319fd625fc2a6d294787582d5a08aa2f
Elvis Boves <eboves@gmail.com>
2023-06-21 14:23:14 -0400

    Working with functions

--- function_practice.py
+++ function_practice.py
@@ -1,1 +1,22 @@
-print("Hello World!")
+#def greetings():
+#    print("Hello")
+#    print("How are you today?")
+#    print("Bye")
+#
+#greetings()
+
+############################# TAKING PARAMETERS ##################################
+
+#def greet(name):
+#    print(f"Hello {name}")
+#
+#
+#greet("Elvis")
+
+
+############################# TAKING MORE THAN 1 PARAMETERS ##################################
+
+def greets(name, location):
+    print(f"Hi {name}, you are currently in {location}")
+
+greets("Elvis", "Dominican Republic")
