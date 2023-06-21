8b124eb534af310895a71d853b240aa6f125d662
Elvis Boves <eboves@gmail.com>
2023-06-21 07:05:58 -0400

    check if user guessed it wrong and reduce live.

--- hangManGame.py
+++ hangManGame.py
@@ -11,14 +11,25 @@ for _ in range(0, len(chosen_word)):
 print(display)
 print(chosen_word)
 
+#lives = len(chosen_word)
+lives = 6
 
-guess = input("Guess a letter: ").lower()
+end_of_game = False
+while not end_of_game:
+    guess = input("Guess a letter: ").lower()
 
-for l in range(0, len(chosen_word)):
-    if guess == chosen_word[l]:
-        display[l] = chosen_word[l]
-        print("right")
-    else:
-        print("wrong")
+    for l in range(0, len(chosen_word)):
+        if guess == chosen_word[l]:
+            display[l] = chosen_word[l]
+            print("right")
+    if guess not in chosen_word[l]:
+        lives -= 1
+        print(lives)
+    if lives == 0:
+        print("You Lost!")
+        break
+    if "_" not in display:
+        end_of_game = True
+        print("You WON!.")
 
 print(display)
