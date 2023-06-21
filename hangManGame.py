3fb6feecf09d0755fc950d4ab45965b16044ec4a
Elvis Boves <eboves@gmail.com>
2023-06-21 07:53:35 -0400

    done!

--- hangManGame.py
+++ hangManGame.py
@@ -6,6 +6,7 @@ import hangman_words
 display = []
 word_list = ["camel", "baboon", "food"]
 
+# This section make a random selection from the list imported from hangman_words
 chosen_word = random.choice(hangman_words.word_list)
 
 for _ in range(0, len(chosen_word)):
@@ -25,10 +26,11 @@ while not end_of_game:
 
     if guess in display:
         print("Already picked!")
-
+    # This is the for loop to check if a guess char is part of the hidden word
     for l in range(0, len(chosen_word)):
         if guess == chosen_word[l]:
             display[l] = chosen_word[l]
+    # This section check if the char selected is not part of the word
     if guess not in chosen_word:
         print(f"{guess} is not a part of the word. You lose a life")
         lives -= 1
@@ -38,6 +40,7 @@ while not end_of_game:
 
     print(hangman_art.stages[lives])
 
+    # This section check if the user have guessed the hidden word
     print(f"{' '.join(display)}")
     if "_" not in display:
         end_of_game = True
