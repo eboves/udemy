eee44247425503a58e9c4dec5f26ecf15753f8a8
Elvis Boves <eboves@gmail.com>
2023-06-22 10:15:36 -0400

    Finish Area Calc project

--- areaCalc.py
+++ areaCalc.py
@@ -1,1 +1,14 @@
-print("Hello Area calc")
+test_h = int(input("Height of wall: "))
+test_w = int(input("width of wall: "))
+
+coverage = 5
+
+def paint_calc(height, width, cover):
+    area = height * width
+    paint = round(area / cover)
+    print(f"You will need {paint} cans of paint.")
+    
+
+
+paint_calc(height=test_h, width=test_w, cover=coverage)
+#paint_calc(height=2, width=4, cover=coverage)
