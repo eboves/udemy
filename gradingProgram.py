14f2c54d4d891d4cee94e116f3d0d1f85a1c055d
Elvis Boves <eboves@gmail.com>
2023-06-27 07:02:31 -0400

    Finish grading program.

--- gradingProgram.py
+++ gradingProgram.py
@@ -1,1 +1,21 @@
-print("Hello from Grading")
+student_scores = {
+    "Harris": 81,
+    "Ron": 78,
+    "Hermione": 99,
+    "Draco": 74,
+    "Neville": 62
+}
+
+student_grades = {}
+
+for key in student_scores:
+    if 90 < student_scores[key] < 100:
+        student_grades[key] = "Outstanding"
+    elif 80 < student_scores[key] <= 90:
+        student_grades[key] = "Exceeds Expectations"
+    elif 70 < student_scores[key] <= 80:
+        student_grades[key] = "Acceptable"
+    else:
+        student_grades[key] = "Fail"
+
+print(student_grades)
