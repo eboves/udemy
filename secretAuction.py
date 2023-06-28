b9a2d9da565d13c8b18f13ac4df25fb59dc63b79
Elvis Boves <eboves@gmail.com>
2023-06-28 09:31:49 -0400

    printing highest bid.

---
+++ secretAuction.py
@@ -0,0 +1,20 @@
+name = input("Please Enter you Name:\n").title()
+bid = int(input("Please Enter you Bid $:\n"))
+
+bidders = {}
+
+while
+def secret_action(name_bidder, bid_amount):
+    bidders[name_bidder] = bid_amount
+    #holder = 0
+    #for key in bidders:
+    #    if bidders[key] > holder:
+    #        holder = bidders[key]
+    #    print(f"The highest bidder is {name_bidder} with a bid of ${holder}")
+
+
+    
+secret_action(name, bid)
+print()
+
+
