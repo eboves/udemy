a017c540fc587cffd32ad5d5fa7d2e9de08dc3f7
Elvis Boves <eboves@gmail.com>
2023-06-28 10:32:20 -0400

    Add a different way to write the program.

--- secretAuction.py
+++ secretAuction.py
@@ -1,5 +1,8 @@
+import art_secret_auction
 
-
+from art_secret_auction import logo
+#print(art_secret_auction.logo)
+print(logo)
 bidders = {}
 keep_playing = True
 # This code add and look for the highest bidder.
@@ -20,4 +23,47 @@ while keep_playing:
     if another_bidder == 'no':
         keep_playing = False
         secret_action(name, bid)
+
+##### A DIFFERENT WAY TO DO IT ########
+# def find_highest_bidder(bids):
+#     highest_bid = 0
+#     winner = ""
+#     for bidder in bids:
+#         bid_amount = bids[bidder]
+#         if bid_amount > highest_bid:
+#             highest_bid = bid_amount
+#             winner = bidder
+
+#     print(f"The highest bidder is {winner} with a bid of ${highest_bid}")  
+
+# bids = {}
+
+# bidding_finished = False
+# while not bidding_finished:
+#     name = input("Please Enter you Name: ").title()
+#     bid = int(input("Please Enter you Bid $"))
+
+#     bids[name] = bid
+#     should_continue = input("Are there any other bidders? Type 'yes' or 'no'.\n").lower()
+#     if should_continue == "no":
+#         bidding_finished = True
+#         find_highest_bidder(bids)
+#     elif should_continue == "yes":
+#         print("Screen clean")
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
