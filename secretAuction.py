9405ebf45c8c92f671f8995ad05b5fbe73decef5
Elvis Boves <eboves@gmail.com>
2023-06-28 10:10:09 -0400

    Finish Secre Auction.

--- secretAuction.py
+++ secretAuction.py
@@ -1,20 +1,21 @@
-name = input("Please Enter you Name:\n").title()
-bid = int(input("Please Enter you Bid $:\n"))
+
 
 bidders = {}
-
-while
+keep_playing = True
 def secret_action(name_bidder, bid_amount):
     bidders[name_bidder] = bid_amount
-    #holder = 0
-    #for key in bidders:
-    #    if bidders[key] > holder:
-    #        holder = bidders[key]
-    #    print(f"The highest bidder is {name_bidder} with a bid of ${holder}")
+    holder = 0
+    for key in bidders:
+        if bidders[key] > holder:
+            holder = bidders[key]
+    print(f"The highest bidder is {name_bidder} with a bid of ${holder}")  
 
-
-    
-secret_action(name, bid)
-print()
-
+while keep_playing:
+    name = input("Please Enter you Name: ").title()
+    bid = int(input("Please Enter you Bid $"))
+    secret_action(name, bid)
+    another_bidder = input("Are there any other bidders? Type 'yes' or 'no'.\n").lower()
+    if another_bidder == 'no':
+        keep_playing = False
+        secret_action(name, bid)
 
