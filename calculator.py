df8347d8ffdb55b8dc11d8c85a6d4d492f4c894a
Elvis Boves <eboves@gmail.com>
2023-06-29 07:09:38 -0400

    Getting user input.

--- calculator.py
+++ calculator.py
@@ -15,3 +15,16 @@ def multiply(n1, n2):
 #divide
 def divide(n1, n2):
     return n1 / n2
+
+operations = {
+    "+": add,
+    "-": substract,
+    "*": multiply,
+    "/": divide,
+}
+
+#num1 = int(input("Please enter the First number: "))
+#num2 = int(input("Please enter the Second number: "))
+
+for operants in operations:
+    print(operants)
