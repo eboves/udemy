4b59328a8706f6261d3f647f31c7f3c347b14996
Elvis Boves <eboves@gmail.com>
2023-06-29 06:57:06 -0400

    Init Calculator project.

---
+++ calculator.py
@@ -0,0 +1,17 @@
+# This is a calculator
+
+#ADD
+def add(n1, n2):
+    return n1 + n2
+
+#Substract
+def substract(n1, n2):
+    return n1 - n2
+
+#Multiply
+def multiply(n1, n2):
+    return n1 * n2
+
+#divide
+def divide(n1, n2):
+    return n1 / n2
