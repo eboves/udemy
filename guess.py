585c4ea6c83f8f39b59e630c3305fb413ae4ea35
Elvis Boves <elvisboves@Elviss-MBP.attlocal.net>
2023-09-12 20:43:36 -0400

    finished game

--- guess.py
+++ guess.py
@@ -1,48 +1,43 @@
 import random
 
 rand = random.randint(1, 100)
-attempts = 0
+attemps = 0
 
-
-
-print(rand)
 print('Welcome to the Number Guessing Game!')
 print("I'm thinking of a number between 1 and 100")
 print(f"Pssst the correct answer is {rand}")
 
-
-
 dificulty = input("Choose a dificulty. Type 'easy' or 'hard': ")
 
+def guess_again(attemps):
+    print("Guess again")
+    print(f"You have {attemps} remaning to guess the number")
 
+def game_on(attemps):
+    is_game = True
+    print(f"You have {attemps} attempts")
+    while is_game:
+        guess_num = int(input("Make a guess: "))
+        if attemps == 0:
+            print("You've run out of guesses, You lose.")
+            return
+        if guess_num == rand:
+            print(f"You got it, the answer was {rand}")
+            is_game = False
+        elif guess_num < rand:
+            attemps -= 1
+            print("Too low.")
+            guess_again(attemps)   
+        else:
+            attemps -= 1
+            print("Too high.")
+            guess_again(attemps)   
 
 
 if dificulty != 'easy':
-    global attempts = 5
-    print("You have {attemps} attempts")
-    guess_num = input("Make a guess: ")
+    attemps = 5
+    game_on(attemps)
+else:
+    attemps = 10
+    game_on(attemps)
 
-
-
-
-"""
-TODO:
-0- if attempts == 0:
-    you;ve run out of guesses, you loose
-1- if easy attempts = 10 if hard attempts = 5
-2- if input is lower than the random number :
-    print(too low)
-    print(guess again)
-    print(you have {attempts} - 1 remaning to guess the number.)
-    make a guess: 
-
-3- if input is greater than random number :
-    print(too high)
-    print(guess again)
-    print(you have {attempts} - 1 remaning to guess the number.)
-    make a guess: 
-
-
-4- else:
-    print(You got it, the answer was {rand})
-"""
