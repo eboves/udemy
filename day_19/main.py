f665f8bcb605d91649fa0d424bc80e4f3f354a55
Elvis Boves <elvisboves@Elviss-MacBook-Pro.local>
2024-05-23 19:53:12 -0400

    Finish the drawing game

---
+++ day_19/main.py
@@ -0,0 +1,38 @@
+from turtle import Turtle, Screen
+
+titi = Turtle()
+screen = Screen()
+
+"""
+thi is a game of drawing something in the screen
+
+
+# def move_forward():
+#     titi.forward(10)
+
+# def move_backward():
+#     titi.backward(10)
+
+# def clock_wise():
+#     titi.right(10)
+#     # or heading_new = titi.heading() + 10 ==> titi.setheading(heading_new)
+
+# def counter_clock_wise():
+#     titi.left(-10)
+
+# def clear_all():
+#     titi.penup()
+#     titi.clear()
+#     titi.setposition(0,0)
+#     titi.seth(0) # or titi.home()
+#     titi.pendown()
+
+# screen.listen()
+# screen.onkey(key="w", fun=move_forward)
+# screen.onkey(key="b", fun=move_backward)
+# screen.onkey(key="d", fun=clock_wise)
+# screen.onkey(key="a", fun=counter_clock_wise)
+# screen.onkey(key="c", fun=clear_all)
+
+"""
+screen.exitonclick()
